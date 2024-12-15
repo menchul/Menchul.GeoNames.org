e9a4df6b41b1e92575bb02f2ec83267e74373e74
IvanP <jonilviv@gmail.com>
2024-12-15 21:17:26 +0100

    Added additional vars to the CMD files.

--- deploy_MSSQL.cmd
+++ deploy_MSSQL.cmd
@@ -1,13 +1,14 @@
-set GeoNamesOrg__ConnectionStrings=Server=localhost;Database=GeoNames.org;Persist Security Info=True;User ID=sa;Password=<PASSWORD>;Connect Timeout=10;Encrypt=False;Trust Server Certificate=True;
+set GeoNamesOrg__ConnectionStrings=Server=localhost;Database=GeoNames.org;Persist Security Info=True;User ID=sa;Password=1qaz@WSX;Connect Timeout=10;Encrypt=False;Trust Server Certificate=True;
 set prj=.\src\Menchul.GeoNames.org\Menchul.GeoNames.org.csproj
 set src=.\src\Menchul.GeoNames.org.MSSQL\Menchul.GeoNames.org.MSSQL.csproj
 set pkj=Microsoft.EntityFrameworkCore.SqlServer
+set net=--framework NET9.0
 
-dotnet add "%prj%" package %pkj% --version 9.0.0 --framework NET9.0
+dotnet add "%prj%" package %pkj% --version 9.0.0 %net%
 
-dotnet ef migrations add InitialMigration -p %prj% -s %src% --framework NET9.0
+dotnet ef migrations add InitialMigration -p %prj% -s %src% %net%
 
-dotnet ef database update -p "%prj%" -s "%src%" --framework NET9.0
+dotnet ef database update -p "%prj%" -s "%src%" %net%
 
 del /F /S /Q ".\src\Menchul.GeoNames.org\Migrations"
 
--- deploy_PostgreSQL.cmd
+++ deploy_PostgreSQL.cmd
@@ -1,13 +1,14 @@
-set GeoNamesOrg__ConnectionStrings=Host=localhost;Port=5432;Database=GeoNames.org;Username=postgres;Password=<PASSWORD>;CommandTimeout=10;
+set GeoNamesOrg__ConnectionStrings=Host=localhost;Port=5433;Database=GeoNames.org;Username=postgres;Password=1qaz@WSX;CommandTimeout=10;
 set prj=.\src\Menchul.GeoNames.org\Menchul.GeoNames.org.csproj
 set src=.\src\Menchul.GeoNames.org.PostgreSQL\Menchul.GeoNames.org.PostgreSQL.csproj
 set pkj=Npgsql.EntityFrameworkCore.PostgreSQL
+set net=--framework NET9.0
 
-dotnet add "%prj%" package %pkj% --version 9.0.2 --framework NET9.0
+dotnet add "%prj%" package %pkj% --version 9.0.2 %net%
 
-dotnet ef migrations add InitialMigration -p %prj% -s %src% --framework NET9.0
+dotnet ef migrations add InitialMigration -p %prj% -s %src% %net%
 
-dotnet ef database update -p "%prj%" -s "%src%" --framework NET9.0
+dotnet ef database update -p "%prj%" -s "%src%" %net%
 
 del /F /S /Q ".\src\Menchul.GeoNames.org\Migrations"
 

ebd05ccb7f99f7532843e46e546ee0b457ecc98f
jonilviv <jonilviv@gmail.com>
2026-02-02 09:18:57 +0100

    Fixed wrong code

--- src/Menchul.GeoNames.org.SQLite/GeoNamesOrgSQLiteDbContext.cs
+++ src/Menchul.GeoNames.org.SQLite/GeoNamesOrgSQLiteDbContext.cs
@@ -12,18 +12,5 @@ namespace Menchul.GeoNames.org.SQLite
             : base(options)
         {
         }
-
-        protected override void OnModelCreating(ModelBuilder modelBuilder)
-        {
-            base.OnModelCreating(modelBuilder);
-
-            // Firebird does not support schemas; clear schema for all entity types
-            modelBuilder.HasDefaultSchema(null);
-
-            foreach (var entityType in modelBuilder.Model.GetEntityTypes())
-            {
-                entityType.SetSchema(null);
-            }
-        }
     }
 }
